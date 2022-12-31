26f53e5ae4608489f683b4a96f3be4e040f9d9b2
DevElou <lemailaelou@gmail.com>
2022-12-31 19:37:00 +0100

    calc_solde,write_solde,save,add_line

--- Calc_read.py
+++ Calc_read.py
@@ -6,23 +6,29 @@ import numpy as np
 import pandas as pd
 
 # Ouverture du ficher
-file = pd.read_excel("test2.xlsx")
 
-data = pd.DataFrame(file, columns=['Catégorie Dépenses', 'Débit', 'Crédit','SOLDE'])
-data = data.fillna(0)
+fichier = "Suivi.xlsx"
+
+def open(name):
+    file = pd.read_excel(name)
+    data = pd.DataFrame(file, columns=['Catégorie Dépenses', 'Débit', 'Crédit', 'SOLDE'])
+    data = data.fillna(0)
+    return data
+
+data = open(fichier)
 
 # Fonctions
 
 def credit(open_file):
     cred = 0
-    for ele in data.values:
+    for ele in open_file.values:
         cred += ele[2]
     return cred
 
 
 def debit(open_file):
     deb = 0.0
-    for ele in data.values:
+    for ele in open_file.values:
         if type(ele[1]) == int:
             ele[1] = float(ele[1])
         if ele[1] != np.nan and type(ele[1]) == float or type(ele[1]) == float:
@@ -30,19 +36,18 @@ def debit(open_file):
             deb = deb + ele[1]
     return deb
 
-
 def categorie_dict(open_file):
     dict = {}
-    for ele in data.values:
+    for ele in open_file.values:
         if not(ele[0] in dict.keys()) and ele[0] != 0:
             dict[ele[0]] = None
     return dict
 
 
-def depense_dict(dict):
+def depense_dict(dict,open_file):
     for cat in dict.keys():
         dep = 0.0
-        for ele in data.values:
+        for ele in open_file.values:
             if ele[0] == cat :
                 if ele[2] > 0.0:
                     dep += float(ele[2])
@@ -51,6 +56,19 @@ def depense_dict(dict):
         dict[cat] = dep
     return dict
 
+def calc_solde(open_file,Line):
+    line = open_file.loc[[Line]]
+    line_preced = open_file.loc[[Line-1]]
+    solde = 0
+    if line.values[0][1] == 0:
+        solde = line.values[0][2] + line_preced.values[0][3]
+    elif line.values[0][2] == 0:
+        solde = line_preced.values[0][3] - line.values[0][1]
+
+    return solde
 
 
-print(depense_dict(categorie_dict(data)))
+
+
+
+
--- Calc_write.py
+++ Calc_write.py
@@ -1,4 +1,23 @@
+import Calc_read as read
 
-import numpy as np
-import pandas as pd
+
+def write_solde(open_file,Line):
+    open_file.loc[Line, ['SOLDE']] = read.calc_solde(open_file,Line)
+
+def save(open_file,name):
+    open_file.to_excel(f"{name}.xlsx")
+
+def add_line(open_file,cat,deb,cred):
+    max_ind = open_file.index[-1]
+    open_file.loc[max_ind+1] = [cat,deb,cred,0]
+
+
+
+
+
+add_line(read.data,"Alimentation",0,10)
+write_solde(read.data,read.data.index[-1])
+print(read.data)
+
+
 
