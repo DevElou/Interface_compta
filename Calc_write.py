0c297fb89bfbf1e950ceca82e373b2c6f522728e
DevElou <lemailaelou@gmail.com>
2023-01-01 23:58:35 +0100

    Main: add line + import + sauv

--- Calc_write.py
+++ Calc_write.py
@@ -1,4 +1,5 @@
 import Calc_read as read
+import os
 
 
 def write_solde(open_file,Line):
@@ -6,7 +7,9 @@ def write_solde(open_file,Line):
 
 def save(open_file,name):
     open_file.to_excel(f"{name}.xlsx")
-
+def save_path(open_file,name,path):
+    out_path = f"{path}\{name}.xlsx"
+    open_file.to_excel(out_path)
 def add_line(open_file,date,lib,cat,deb,cred):
     max_ind = open_file.index[-1]
     open_file.loc[max_ind+1] = [date,lib,cat,0,deb,cred,0]
@@ -15,9 +18,7 @@ def add_line(open_file,date,lib,cat,deb,cred):
 
 
 
-add_line(read.data,"00/00/00","Test","Alimentation",0,10)
-write_solde(read.data,read.data.index[-1])
-print(read.data)
+
 
 
 
--- Main.py
+++ Main.py
@@ -1,5 +1,33 @@
 from tkinter import *
-#import Calc_write
+from tkinter import filedialog
+import Calc_write as fct
+import pathlib
+
+
+#Ouvre fichier et read.open dans global var
+def ouvrir():
+    open_path = filedialog.askopenfilename(title="Ouvrir un fichier de compte",defaultextension=".xlsx",filetypes=[("Xlsx Fichier",".xlsx")])
+    global path
+    path = pathlib.Path(open_path).parent.resolve()
+    global data
+    data = fct.read.open(open_path)
+
+
+def save():
+    try:
+        data
+    except NameError:
+        err.config(text="Veuillez ouvrir un fichier")
+    else:
+        err.config(text="")
+        fct.save_path(data,"Sauv1",path)
+
+
+def add_line(data):
+    fct.add_line(data, date_entry.get(),str(libelle_entry.get()),str(cat_entry.get()),int(Debit_entry.get()),int(Credit_entry.get()))
+    last_line = len(data) -1
+    fct.write_solde(data,last_line)
+
 
 # Creation de la fenêtre
 fenetre = Tk()
@@ -9,10 +37,17 @@ fenetre.resizable(width=False, height=False)
 
 # Frame ajout de ligne
 add_Frame = Frame(fenetre)
-add_Frame.config(width=640,height=360)
+add_Frame.config(width=640,height=360,highlightbackground="black",highlightthickness=1)
 add_Frame.grid(row=0,column=0,sticky="NW")
 
+#Frame import fichier
+import_Frame = Frame(fenetre)
+import_Frame.config(width=640,height=360,highlightbackground="black",highlightthickness=1)
+import_Frame.grid(row=1,column=0,sticky="SW")
+
+
 #Contenu add_Frame
+#Ajouter aide pr indique mettre 0 credit ou debit
 add_titre = Label(add_Frame,text="Ajouter un débit ou crédit",font=("Helvetica",15,"bold"))
 add_titre.place(relx=0.5, rely=0.05, anchor=CENTER)
 
@@ -29,17 +64,51 @@ libelle_entry = Entry(add_Frame)
 libelle_lab.place(relx=0.436,rely=0.26,anchor=CENTER)
 libelle_entry.place(relx=0.5,rely=0.31,anchor=CENTER)
 
-
+#add catégorie
 cat_lab = Label(add_Frame,text="Catégorie Dépenses : ")
-date_entry = Entry(add_Frame)
+cat_entry = Entry(add_Frame)
+cat_lab.place(relx=0.49,rely=0.38,anchor=CENTER)
+cat_entry.place(relx=0.5,rely=0.43,anchor=CENTER)
+
+#add pointé
 Pointe_lab = Label(add_Frame,text="Pointé : ")
-date_entry = Entry(add_Frame)
+Pointe_entry = Entry(add_Frame)
+Pointe_lab.place(relx=0.439,rely=0.49,anchor=CENTER)
+Pointe_entry.place(relx=0.5,rely=0.54,anchor=CENTER)
+
+
+#add debit
 Debit_lab = Label(add_Frame,text="Débit : ")
-date_entry = Entry(add_Frame)
+Debit_entry = Entry(add_Frame)
+Debit_lab.place(relx=0.434,rely=0.60,anchor=CENTER)
+Debit_entry.place(relx=0.5,rely=0.65,anchor=CENTER)
+
+#add credit
 Credit_lab = Label(add_Frame,text="Crédit : ")
-date_entry = Entry(add_Frame)
-Solde_lab = Label(add_Frame,text="Solde : ")
-date_entry = Entry(add_Frame)
+Credit_entry = Entry(add_Frame)
+Credit_lab.place(relx=0.434,rely=0.71,anchor=CENTER)
+Credit_entry.place(relx=0.5,rely=0.76,anchor=CENTER)
+
+#Bouton add_line
+btn_add = Button(add_Frame,text="Ajouter la ligne",command= lambda: add_line(data))
+btn_add.place(relx=0.5,rely=0.86,anchor=CENTER)
+
+#Contenu import_frame
+#Ajouter label importance etape 1
+import_titre = Label(import_Frame,text="Importer un fichier de compte",font=("Helvetica",15,"bold"))
+import_titre.place(relx=0.5, rely=0.05, anchor=CENTER)
+
+#Label error(import_Frame)
+err = Label(import_Frame,text="",font=("Helvetica",10),fg='red')
+err.place(relx=0.5, rely=0.55, anchor=CENTER)
+
+#Boutton ouvrir fichier(import_Frame)
+btn_import = Button(import_Frame,text="Ouvrir un fichier de compte",command=ouvrir)
+btn_import.place(relx=0.5,rely=0.49,anchor=CENTER)
+
+#Boutton save (import_Frame)
+btn_save = Button(import_Frame,text=" Sauvegarder le fichier ",command=save)
+btn_save.place(relx=0.5,rely=0.96,anchor=CENTER)
 
 
 fenetre.mainloop()
